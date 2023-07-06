2dd736c216d1d00931042354e01a7315445f4b5b
Michelle Castaneda <michellesaucedas@gmail.com>
2023-07-06 16:26:05 -0500

    Movie List HTML JS

---
+++ index.js
@@ -0,0 +1,37 @@
+let message = document.querySelector('#message')
+
+let addMovie = (event) => {
+    event.preventDefault();
+    let inputField = document.querySelector('input');
+    let movie = document.createElement('li');
+    let movieTitle = document.createElement ('span');
+    movieTitle.textContent = inputField.value;
+    movieTitle.addEventListener('click',crossOffMovie);
+    movie.appendChild(movieTitle);
+
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'x';
+    deleteBtn.addEventListener('click', deleteMovie)
+    movie.appendChild(deleteBtn);
+
+    let ulElement = document.querySelector('ul');
+    ulElement.appendChild(movie);
+    inputField.value = ""
+}
+
+let form = document.querySelector('form')
+form.addEventListener('submit', addMovie);
+
+let deleteMovie = (event) => {
+    event.target.parentNode.remove()
+    message.textContent='Movie Deleted'
+}
+
+let crossOffMovie = (event) => {
+    event.target.classList.toggle('checked') 
+    if(event.target.classList.contains('checked')) {
+        message.textContent ='Movie Watched!'
+    } else {
+        message.textContent = 'Movie Added Back!'
+    }
+}
